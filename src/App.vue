9b581b8a4b0b73383f5266703f2849d0542b9652
tower1229 <tower1229@foxmail.com>
2018-08-28 16:23:44 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-upload class="myUpload"
+      drag
+      action=""
+      accept="application/json"
+      :before-upload="getFile">
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">导入JSON文件</div>
+    </el-upload>
+    <div class="m">
+      <el-input
+        type="textarea"
+        :rows="10"
+        placeholder="请输入内容"
+        v-model="fileText"
+        @change="saveFile">
+      </el-input>
+    </div>
+    <a id="Download" download="download.json" target="_blank" class="DownloadArea">保存JSON文件</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      fileText: null,
+      DownloadDom: null
+    };
+  },
+  methods: {
+    readLocalFile: function(file, callback) {
+      var reader = new FileReader();
+      //将文件以文本形式读入页面
+      reader.readAsText(file);
+      reader.onload = function() {
+        typeof callback === "function" && callback(this.result);
+      };
+    },
+    saveFile: function() {
+      if (this.DownloadDom) {
+        let blobStr = [this.fileText];
+        var myBlob = new Blob(blobStr, { type: "application/json" });
+        this.DownloadDom.href = window.URL.createObjectURL(myBlob);
+        console.log('下载文件已就绪')
+      }
+    },
+    getFile: function(file) {
+      this.readLocalFile(file, result => {
+        this.fileText = result.replace(/\s/g, "");
+        this.saveFile();
+      });
+      return false;
+    }
+  },
+  mounted: function() {
+    this.DownloadDom = document.getElementById("Download");
   }
-}
+};
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 960px;
+  height: 100%;
+}
+.m {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.myUpload .el-upload {
+  display: block;
+}
+.myUpload .el-upload-dragger {
+  width: 100%;
+}
+
+.DownloadArea {
+  display: block;
+  height: 100px;
+  line-height: 100px;
+  border: 1px dashed #ccc;
+  background: #f5f5f5;
+  transition: background ease 0.5s;
+  cursor: pointer;
+  border-radius: 6px;
+  text-decoration: none;
+  color:#333;
+}
+.DownloadArea:hover {
+  background: #fff;
 }
 </style>
